b7c92bb426c6bca52a6dd44e6df41e19b9356b16
acdalusong <acdalusong@up.edu.ph>
2024-11-07 19:56:54 +0800

    Update new_financial_transaction.py

--- new_financial_transaction.py
+++ new_financial_transaction.py
@@ -1,13 +1,16 @@
+from urllib.parse import parse_qs, urlparse
 import dash
 import dash_bootstrap_components as dbc
-from dash import Input, Output, State, dcc, html
-from dash.exceptions import PreventUpdate
+from dash import dcc, html
 
+from dash import Input, Output, State
 from app import app
 from apps.dbconnect import getDataFromDB, modifyDB
+from dash.exceptions import PreventUpdate
 
 layout = html.Div(
     [
+        dcc.Store(id='financial_transaction_id', storage_type='memory', data=0),
         # Header with Back Button
         dbc.Row(
             [
@@ -34,7 +37,7 @@ layout = html.Div(
         
         html.Hr(),
 
-        dbc.Alert(id='transactionprofile_alert', is_open=False), # For feedback purposes
+        # dbc.Alert(id='financial_transaction_alert', is_open=False), # For feedback purposes
 
         # Form Layout
         dbc.Form(
@@ -162,12 +165,24 @@ layout = html.Div(
                     className="mb-3"
                 ),
 
+                # Mark for Deletion
+                html.Div(
+                    [
+                        dbc.Checklist(
+                            id='financial_transaction_delete',
+                            options=[dict(value=1, label="Mark as Deleted")],
+                            value=[]
+                        )
+                    ],
+                    id='financial_transaction_deletediv'
+                ),
+
                 # Submit Button
                 dbc.Button(
                     "Submit",
+                    id='submit_button',
                     color="primary", 
                     className="mt-3",
-                    n_clicks=0, # Initialize number of clicks
                     style={
                         "borderRadius": "20px",
                         "fontWeight": "bold",
@@ -175,9 +190,11 @@ layout = html.Div(
                         "backgroundColor": "#194D62",
                         "color": "white"
                     },
+                    n_clicks=0, # Initialize number of clicks
                 )
             ]
-        )
+        ),
+        dbc.Alert(id='submit_alert', is_open=False)
     ],
     className="container mt-4"
 )
@@ -214,6 +231,122 @@ def load_treatment_names(_):
     
     # Return data as options for dropdown
     return [{"label": row["treatment_name"], "value": row["treatment_id"]} for _, row in df.iterrows()]
+
+
+@app.callback(
+    [
+        Output('financial_transaction_id', 'data'),
+        Output('financial_transaction_deletediv', 'className')
+    ],
+    [Input('url', 'pathname')],
+    [State('url', 'search')]
+)
+def financial_transaction_load(pathname, urlsearch):
+    if pathname == '/financial_transaction_management/new_transaction':
+        parsed = urlparse(urlsearch)
+        create_mode = parse_qs(parsed.query).get('mode', [''])[0]
+
+        if create_mode == 'add':
+            financial_transaction_id = 0
+            financial_transaction_deletediv = 'd-none'
+            
+        else:
+            financial_transaction_id = int(parse_qs(parsed.query).get('id', [0])[0])
+            financial_transaction_deletediv =''
+        
+        return [financial_transaction_id, financial_transaction_deletediv]
+    else:
+        raise PreventUpdate
+
+# @app.callback(
+#     [Output('submit_alert', 'color'),
+#      Output('submit_alert', 'children'),
+#      Output('submit_alert', 'is_open')],
+#     [Input('submit_button', 'n_clicks')],
+#     [State('patient_name_dropdown', 'value'),
+#      State('treatment_name_dropdown', 'value'),
+#      State('financialtransaction_date', 'value'),
+#      State('financialtransaction_paymentamount', 'value'),
+#      State('financialtrasaction_status', 'value'),
+#      State('financialtransaction_paidamount', 'value'),
+#      State('financialtransaction_remarks', 'value'),
+#      State('url', 'search'),
+#      State('financial_transaction_id', 'data')]
+# )
+# def submit_form(n_clicks, patient_name_dropdown, treatment_name_dropdown, financialtransaction_date, 
+#                 financialtransaction_paymentamount, financialtrasaction_status, financialtransaction_paidamount,
+#                 financialtransaction_remarks, urlsearch, financial_transaction_id):
+    
+#     ctx = dash.callback_context
+#     if not ctx.triggered or not n_clicks:
+#         raise PreventUpdate
+
+#     parsed = urlparse(urlsearch)
+#     create_mode = parse_qs(parsed.query).get('mode', [''])[0]
+
+#     # Check for missing values in the required fields
+#     if not all([patient_name_dropdown, treatment_name_dropdown, financialtransaction_date,
+#                 financialtransaction_paymentamount, financialtrasaction_status, financialtransaction_paidamount,
+#                 financialtransaction_remarks]):
+#         return 'danger', 'Please fill in all required fields.', True
+
+    # # SQL to insert or update the database
+    # if create_mode == 'add':
+    #     sql = """INSERT INTO patient (patient_last_m, patient_first_m, patient_middle_m, patient_bd, age,
+    #             sex, patient_cn, patient_email, street, barangay, city, postal_code, account_creation_date)
+    #             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, CURRENT_DATE);"""
+        
+    #     values = [last_name, first_name, middle_name, birthdate, age, sex, cellphone_number, 
+    #               email_address, street, barangay, city, postal_code]
+    
+    # elif create_mode == 'edit':
+    #     sql = """UPDATE patient
+    #             SET patient_last_m = %s,
+    #                 patient_first_m = %s,
+    #                 patient_middle_m = %s,
+    #                 patient_bd = %s,
+    #                 age = %s,
+    #                 sex = %s,
+    #                 patient_cn = %s,
+    #                 patient_email = %s,
+    #                 street = %s,
+    #                 barangay = %s,
+    #                 city = %s,
+    #                 postal_code = %s
+    #             WHERE patient_id = %s;"""
+        
+    #     values = [last_name, first_name, middle_name, birthdate, age, sex, cellphone_number, 
+    #               email_address, street, barangay, city, postal_code, patientprofile_id]
+    # else:
+    #     raise PreventUpdate
+
+    # try:
+    #     modifyDB(sql, values)
+    #     return 'success', 'Patient Profile Submitted successfully!', True
+    # except Exception as e:
+    #     return 'danger', f'Error Occurred: {e}', True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         # dbc.Modal( # Modal = dialog box; feedback for successful saving.
